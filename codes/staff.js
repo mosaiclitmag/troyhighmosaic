c76ffa11ac6c0dc4f149d16adf636ede965a9317
gyro v <vansterling&@gmail.com>
2024-08-09 20:12:21 -0400

    first commit

---
+++ codes/staff.js
@@ -0,0 +1,224 @@
+function showDiv() { /* please dont touch unless you know what you're doing. This makes the intro on the side appear. they are ordered chronologically. There are some for people who didn't put a description down.*/
+    document.querySelector(".intro1").style.display = "block"; /*1 */
+  }
+  function closeDiv() {
+    document.querySelector(".intro1").style.display = "none";
+  }
+  
+  function showOrHide() {
+    if (document.querySelector(".intro1").style.display === "block") {
+      closeDiv()
+    } else {
+      showDiv()
+    }
+  }
+  function showDiv2() { 
+    document.querySelector(".intro2").style.display = "block"; /*2*/
+  }
+  function closeDiv2() {
+    document.querySelector(".intro2").style.display = "none";
+  }
+  
+  function showOrHide2() {
+    if (document.querySelector(".intro2").style.display === "block") {
+      closeDiv2()
+    } else {
+      showDiv2()
+    }
+  }
+  function showDiv3() { 
+    document.querySelector(".intro3").style.display = "block"; /*3*/
+  }
+  function closeDiv3() {
+    document.querySelector(".intro3").style.display = "none";
+  }
+  
+  function showOrHide3() {
+    if (document.querySelector(".intro3").style.display === "block") {
+      closeDiv3()
+    } else {
+      showDiv3()
+    }
+  }
+  function showDiv4() { 
+    document.querySelector(".intro4").style.display = "block"; /*4*/
+  }
+  function closeDiv4() {
+    document.querySelector(".intro4").style.display = "none";
+  }
+  
+  function showOrHide4() {
+    if (document.querySelector(".intro4").style.display === "block") {
+      closeDiv4()
+    } else {
+      showDiv4()
+    }
+  }
+  function showDiv5() { 
+    document.querySelector(".intro5").style.display = "block"; /*5*/
+  }
+  function closeDiv5() {
+    document.querySelector(".intro5").style.display = "none";
+  }
+  
+  function showOrHide5() {
+    if (document.querySelector(".intro5").style.display === "block") {
+      closeDiv5()
+    } else {
+      showDiv5()
+    }
+  }
+  function showDiv6() { 
+    document.querySelector(".intro6").style.display = "block"; /*6*/
+  }
+  function closeDiv6() {
+    document.querySelector(".intro6").style.display = "none";
+  }
+  
+  function showOrHide6() {
+    if (document.querySelector(".intro6").style.display === "block") {
+      closeDiv6()
+    } else {
+      showDiv6()
+    }
+  }  
+  function showDiv7() { 
+    document.querySelector(".intro7").style.display = "block"; /*7*/
+  }
+  function closeDiv7() {
+    document.querySelector(".intro7").style.display = "none";
+  }
+  
+  function showOrHide7() {
+    if (document.querySelector(".intro7").style.display === "block") {
+      closeDiv7()
+    } else {
+      showDiv7()
+    }
+  } 
+  function showDiv8() { 
+    document.querySelector(".intro8").style.display = "block"; /*8*/
+  }
+  function closeDiv8() {
+    document.querySelector(".intro8").style.display = "none";
+  }
+  
+  function showOrHide8() {
+    if (document.querySelector(".intro8").style.display === "block") {
+      closeDiv8()
+    } else {
+      showDiv8()
+    }
+  }  
+  function showDiv9() { 
+    document.querySelector(".intro9").style.display = "block"; /*9*/
+  }
+  function closeDiv9() {
+    document.querySelector(".intro9").style.display = "none";
+  }
+  
+  function showOrHide9() {
+    if (document.querySelector(".intro9").style.display === "block") {
+      closeDiv9()
+    } else {
+      showDiv9()
+    }
+  }
+  function showDiv10() { 
+    document.querySelector(".intro10").style.display = "block"; /*10*/
+  }
+  function closeDiv10() {
+    document.querySelector(".intro10").style.display = "none";
+  }
+  
+  function showOrHide10() {
+    if (document.querySelector(".intro10").style.display === "block") {
+      closeDiv10()
+    } else {
+      showDiv10()
+    }
+  } 
+  function showDiv11() { 
+    document.querySelector(".intro11").style.display = "block"; /*11*/
+  }
+  function closeDiv11() {
+    document.querySelector(".intro11").style.display = "none";
+  }
+  
+  function showOrHide11() {
+    if (document.querySelector(".intro11").style.display === "block") {
+      closeDiv11()
+    } else {
+      showDiv11()
+    }
+  } 
+  function showDiv12() { 
+    document.querySelector(".intro12").style.display = "block"; /*12*/
+  }
+  function closeDiv12() {
+    document.querySelector(".intro12").style.display = "none";
+  }
+  
+  function showOrHide12() {
+    if (document.querySelector(".intro12").style.display === "block") {
+      closeDiv12()
+    } else {
+      showDiv12()
+    }
+  } 
+  function showDiv13() { 
+    document.querySelector(".intro13").style.display = "block"; /*13*/
+  }
+  function closeDiv13() {
+    document.querySelector(".intro13").style.display = "none";
+  }
+  
+  function showOrHide13() {
+    if (document.querySelector(".intro13").style.display === "block") {
+      closeDiv13()
+    } else {
+      showDiv13()
+    }
+  }
+  function showDiv14() { 
+    document.querySelector(".intro14").style.display = "block"; /*14*/
+  }
+  function closeDiv14() {
+    document.querySelector(".intro14").style.display = "none";
+  }
+  
+  function showOrHide14() {
+    if (document.querySelector(".intro14").style.display === "block") {
+      closeDiv14()
+    } else {
+      showDiv14()
+    }
+  }
+  function showDiv15() { 
+    document.querySelector(".intro15").style.display = "block"; /*15*/
+  }
+  function closeDiv15() {
+    document.querySelector(".intro15").style.display = "none";
+  }
+  
+  function showOrHide15() {
+    if (document.querySelector(".intro15").style.display === "block") {
+      closeDiv15()
+    } else {
+      showDiv15()
+    }
+  }
+  function showDiv16() { 
+    document.querySelector(".intro16").style.display = "block"; /*16*/
+  }
+  function closeDiv16() {
+    document.querySelector(".intro16").style.display = "none";
+  }
+  
+  function showOrHide16() {
+    if (document.querySelector(".intro16").style.display === "block") {
+      closeDiv16()
+    } else {
+      showDiv16()
+    }
+  }
